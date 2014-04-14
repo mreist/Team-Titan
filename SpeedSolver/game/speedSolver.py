4e6f69dd064b9b0ddabcd1fc4045ded67088b424
Jason <jrub@udel.edu>
2014-04-14 15:29:13 -0400

    Merge branch 'Jason-Branch'
    Conflicts:
    SpeedSolver/game/speedSolver.py

--- SpeedSolver/game/speedSolver.py
+++ SpeedSolver/game/speedSolver.py
@@ -23,12 +23,22 @@ class Vehicle(spyral.Sprite):
         self.speed = 0
 
 class Question(spyral.Sprite):
-    def __init__(self, scene, location):
+    def __init__(self, scene, location, operator, digits):
         spyral.Sprite.__init__(self, scene)
-        self.num1 = random.randint(1, 10)
-        self.num2 = random.randint(1, 10)
-        self.answer = self.num1 + self.num2
+        if digits == 1:
+            self.num1 = random.randint(1, 10)
+            self.num2 = random.randint(1, 10)
+        elif digits == 2:
+            self.num1 = random.randint(10, 100)
+            self.num2 = random.randint(10, 100)
+        elif digits == 3:
+            self.num1 = random.randint(100, 1000)
+            self.num2 = random.randint(100, 1000)
+        else:
+            self.num1 = random.randint(1, 10000000)
+            self.num2 = random.randint(1, 10000000)
         self.font = spyral.Font(DEF_FONT, 36)
+<<<<<<< HEAD
 
         self.image = self.font.render(str(self.num1) + "+" + str(self.num2) + "= ?")
 
@@ -41,6 +51,35 @@ class Title(spyral.Sprite):
         self.anchor = 'center'     '
 
 
+=======
+        if operator == 'addition':
+            self.answer = self.num1 + self.num2
+            self.image = self.font.render(str(self.num1) + "+" + str(self.num2) + "= ?")
+        elif operator == 'multiplication':
+            self.answer = self.num1*self.num2
+            self.image = self.font.render(str(self.num1) + "x" + str(self.num2) + "= ?")
+        elif operator == 'subtraction':
+            self.answer = self.num1-self.num2
+            self.image = self.font.render(str(self.num1) + "-" + str(self.num2) + "= ?")
+        elif operator == 'division':
+            checkdivision(self.num1, self.num2)
+        
+    def checkdivision(self, num1, num2):
+        if self.num1 % self.num2 == 0:
+            self.answer = num1/num2
+            self.image = self.font.render(str(self.num1) + "/" + str(self.num2) + "= ?")
+        else:
+            self.num1 = random.randint(1, 10)
+            self.num2 = random.randint(1, 10)
+            checkdivision(num1, num2)
+            
+class Options(spyral.Scene):
+    def __init__(self, *args, **kwargs):
+        spyral.Scene.__init__(self, SIZE)
+        self.background = spyral.Image("libraries/spyral/resources/images/testBackground.png")
+        
+    
+>>>>>>> Jason-Branch
 class SpeedSolver(spyral.Scene):
 	def __init__(self, *args, **kwargs):
 
