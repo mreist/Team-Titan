a1400df0f66a15037c90136084e7fce4405b0ec9
edu374 <edu374@edu374-VirtualBox>
2014-04-18 14:35:11 -0400

    Split up all code, got scene switching to work

--- SpeedSolver/game/speedSolver.py
+++ SpeedSolver/game/speedSolver.py
@@ -2,6 +2,8 @@ import spyral
 import random
 import math
 import pygame
+import Options
+import MainScreen
 
 WIDTH = 1200
 HEIGHT = 900
@@ -10,103 +12,22 @@ WHITE = (255, 255, 255)
 SIZE = (WIDTH, HEIGHT)
 DEF_FONT = "libraries/spyral/resources/fonts/DejaVuSans.ttf"
 
-
-class Vehicle(spyral.Sprite):
-    def __init__(self, scene):
-        spyral.Sprite.__init__(self, scene)
-        
-        self.image = spyral.Image(size=(300, 100))
-        #Based on which vehicle they select use a switch statement to select self.image
-        self.image = spyral.Image("images/Car.png")
-        self.anchor = 'center'     #Default anchor is 'topleft'
-
-        self.speed = 0
-
-class Question(spyral.Sprite):
-    def __init__(self, scene, location, operator, digits):
-        spyral.Sprite.__init__(self, scene)
-        if digits == 1:
-            self.num1 = random.randint(1, 10)
-            self.num2 = random.randint(1, 10)
-        elif digits == 2:
-            self.num1 = random.randint(10, 100)
-            self.num2 = random.randint(10, 100)
-        elif digits == 3:
-            self.num1 = random.randint(100, 1000)
-            self.num2 = random.randint(100, 1000)
-        else:
-            self.num1 = random.randint(1, 10000000)
-            self.num2 = random.randint(1, 10000000)
-            
-        self.font = spyral.Font(DEF_FONT, 36)
-        
-          if operator == 'addition':
-            self.answer = self.num1 + self.num2
-            self.image = self.font.render(str(self.num1) + "+" + str(self.num2) + "= ?")
-        elif operator == 'multiplication':
-            self.answer = self.num1*self.num2
-            self.image = self.font.render(str(self.num1) + "x" + str(self.num2) + "= ?")
-        elif operator == 'subtraction':
-            self.answer = self.num1-self.num2
-            self.image = self.font.render(str(self.num1) + "-" + str(self.num2) + "= ?")
-        elif operator == 'division':
-            checkdivision(self.num1, self.num2)
         
 
-    
-      
-        
-    def checkdivision(self, num1, num2):
-        if self.num1 % self.num2 == 0:
-            self.answer = num1/num2
-            self.image = self.font.render(str(self.num1) + "/" + str(self.num2) + "= ?")
-        else:
-            self.num1 = random.randint(1, 10)
-            self.num2 = random.randint(1, 10)
-            checkdivision(num1, num2)
-            
-class Options(spyral.Scene):
-    def __init__(self, *args, **kwargs):
-        spyral.Scene.__init__(self, SIZE)
-        self.background = spyral.Image("images/Background.png")
-        
-class Title(spyral.Sprite):
-    def __init__(self, scene):
-        spyral.Sprite.__init__(self, scene)
-        
-        self.image = spyral.Image(size=(300, 100))
-        self.image = spyral.Image("images/Title.png")
-        self.anchor = 'center' 
         
 class SpeedSolver(spyral.Scene):
-	def __init__(self, *args, **kwargs):
+	def __init__(self):
 
 		super(SpeedSolver, self).__init__(SIZE)
        
-    		
-		self.background = spyral.Image("images/Background.png")
-		self.playerVehicle = Vehicle(self)
-		self.playerVehicle.pos = (WIDTH/2, (HEIGHT/2)+200)
-    
-		self.Title = Title(self)
-		self.Title.pos = (WIDTH/2, (HEIGHT/2) - 300)
-
-		class RegisterForm(spyral.Form):
-			StartGame = spyral.widgets.Button("Start Game")
-			Options = spyral.widgets.Button("Options")
-    		self.my_form = RegisterForm(self)
-		self.my_form.focus()
-		self.my_form.StartGame.pos = ((WIDTH/2)-50, (HEIGHT/2) + 200)
-		self.my_form.Options.pos = ((WIDTH/2)-50, (HEIGHT/2) + 300)
-		
-		#spyral.event.register("input.mouse.down", self.StartGame)
-		#spyral.event.register("input.mouse.down", self.Options)
-
-   		#def StartGame(self,pos):
-	
-   		#def Options(self,pos):
-	
 		spyral.event.register('input.keyboard.down.esc', spyral.director.pop)
    		spyral.event.register("system.quit", spyral.director.pop)
+  
+
+
+
+
+
+
 
 	
