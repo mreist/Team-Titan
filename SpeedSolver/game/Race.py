5ab94aa8002e29782050967cd5260ec427e48f75
Jason <jrub@udel.edu>
2014-04-21 18:40:33 -0400

    Fixing more text input stuff
    Still not complete

--- SpeedSolver/game/Race.py
+++ SpeedSolver/game/Race.py
@@ -20,12 +20,13 @@ class RaceScene(spyral.Scene):
         spyral.event.register("system.quit", spyral.director.quit)
 
         self.background = spyral.Image("images/Background.png")
-
+        self.level = 1
+        
         playerVehicle = Vehicle.Vehicles(self)
         playerVehicle.pos = (WIDTH/4, (HEIGHT/2)+200)
         
-        currentQuestion = Questions.Question(self, 'addition', 1)
-        currentQuestion.pos = (WIDTH/2, (HEIGHT/2)+435)
+        self.currentQuestion = Questions.Question(self, 'addition', 1)
+        self.currentQuestion.pos = (WIDTH/2, (HEIGHT/2)+435)
         
         class RegisterForm(spyral.Form):
             QuitButton = spyral.widgets.Button("Quit")
@@ -41,23 +42,27 @@ class RaceScene(spyral.Scene):
         self.my_form.QuitButton.pos = ((WIDTH-100), (HEIGHT-50))
         self.my_form.EnterButton.pos = ((WIDTH/2 + 70), (HEIGHT/2)+400)
         self.my_form.AnswerInput.pos = ((WIDTH/2 + 30), (HEIGHT/2)+400)
-        spyral.event.register("form.RegisterForm.QuitButton.clicked", self.goToMenu)
-
         
+        spyral.event.register("form.RegisterForm.QuitButton.clicked", self.goToMenu)
+    
+        spyral.event.register("form.RegisterForm.EnterButton.clicked", self.checkAnswer)
+    
+       
+
+    def checkAnswer(self):
         if self.my_form.AnswerInput.value == currentQuestion.answer:
             print "CORRECT"
         else:
             print "INCORRECT"
-        
-        class RegisterForm(spyral.Form):
-            QuitButton = spyral.widgets.Button("Quit")
+        self.newQuestion
+        self.level += 1
+        if self.level > 10:
+            self.goToMenu
+            #eventually: go to score screen
             
-        self.my_form = RegisterForm(self)
-
-        self.my_form.focus()
-        self.my_form.QuitButton.pos = ((WIDTH-100), (HEIGHT-50))
-
-
+    def newQuestion(self):
+        currentQuestion = Questions.Question(self, 'addition', 1)
+            
     def goToMenu(self):
         spyral.director.pop
         spyral.director.push(MainScreen.MainMenu())
