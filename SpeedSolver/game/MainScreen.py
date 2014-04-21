d922f12e814f91542e1e0a0b75dadd94fa401999
avellen <avellen@udel.edu>
2014-04-21 13:04:32 -0400

    Updated Garage
    When you click garage it goes to that scene

--- SpeedSolver/game/MainScreen.py
+++ SpeedSolver/game/MainScreen.py
@@ -3,8 +3,8 @@ import random
 import math
 import Vehicle
 import Options
+import Race
 import Garage
-
 
 WIDTH = 1200
 HEIGHT = 900
@@ -14,47 +14,43 @@ SIZE = (WIDTH, HEIGHT)
 DEF_FONT = "libraries/spyral/resources/fonts/DejaVuSans.ttf"
 
 class MainMenu(spyral.Scene):
-	def __init__(self):
-		super(MainMenu, self).__init__(SIZE)
+    def __init__(self):
+        super(MainMenu, self).__init__(SIZE)
 
-		spyral.event.register('input.keyboard.down.esc', spyral.director.quit)
-		spyral.event.register("system.quit", spyral.director.quit)
+        spyral.event.register('input.keyboard.down.esc', spyral.director.quit)
+        spyral.event.register("system.quit", spyral.director.quit)
 
-		self.background = spyral.Image("images/Background.png")
-                self.image = spyral.Image("images/Garage.png")
-                
-		playerVehicle = Vehicle.Vehicles(self)
-		playerVehicle.pos = (WIDTH/2, (HEIGHT/2)+200)
+        self.background = spyral.Image("images/Background.png")
+        self.image = spyral.Image("images/Garage.png")
+        playerVehicle = Vehicle.Vehicles(self)
+        playerVehicle.pos = (WIDTH/2, (HEIGHT/2)+200)
+        CarGarage = Garage.Garage(self)
+        CarGarage.pos = ((WIDTH/2), (HEIGHT/2)-200)
 
+        class RegisterForm(spyral.Form):
+            StartGame = spyral.widgets.Button("Start Game")
+            OptionButton = spyral.widgets.Button("Options")
+        
+        self.my_form = RegisterForm(self)
 
-                CarGarage = Garage.Garage(self)
-                CarGarage.pos = ((WIDTH/2), (HEIGHT/2)-200)
+        self.my_form.focus()
+        self.my_form.StartGame.pos = ((WIDTH/2)-50, (HEIGHT/2) + 200)
+        self.my_form.OptionButton.pos = ((WIDTH/2)-50, (HEIGHT/2) + 300)
 
-		class RegisterForm(spyral.Form):
-			StartGame = spyral.widgets.Button("Start Game")
-			OptionButton = spyral.widgets.Button("Options")
-                        GarageButton = spyral.widgets.Button("Enter Garage")
-    		self.my_form = RegisterForm(self)
-
-		self.my_form.focus()
-		self.my_form.StartGame.pos = ((WIDTH/2)-50, (HEIGHT/2) + 200)
-		self.my_form.OptionButton.pos = ((WIDTH/2)-50, (HEIGHT/2) + 300)
-                self.my_form.GarageButton.pos = ((WIDTH/2)-50, (HEIGHT/2) - 200)
-
-		spyral.event.register("form.RegisterForm.OptionButton.clicked", self.goToOptions)
-                spyral.event.register("form.RegisterForm.GarageButton.clicked", self.goToGarage)
-
-
-                #spyral.event.register("input.mouse.down.left", self.goToGarage)
-                
+        spyral.event.register("form.RegisterForm.OptionButton.clicked", self.goToOptions)
+        spyral.event.register("form.RegisterForm.StartGame.clicked", self.goToRace)
+        spyral.event.register("input.mouse.down", self.goToGarage)
+        
+    def goToOptions(self):
+        spyral.director.pop
+        spyral.director.push(Options.OptionScene()) 
 		
-	def goToOptions(self):
-		spyral.director.pop
-		spyral.director.push(Options.OptionScene()) 
-
-        def goToGarage(self):
-               # if pos.x > 300 and pos.x < 600 and pos.y > 350 and pos.y < 500:
-               spyral.director.pop
-               spyral.director.push(Garage.GarageScene())
-
-
+    def goToRace(self):
+        spyral.director.pop
+        spyral.director.push(Race.RaceScene())
+        
+    def goToGarage(self, pos):
+        pos = spyral.Vec2D(pos)
+        if pos.x > 390 and pos.x < 870 and pos.y >50 and pos.y < 475:
+            spyral.director.pop
+            spyral.director.push(Garage.GarageScene())
--- SpeedSolver/game/__init__.py
+++ SpeedSolver/game/__init__.py
@@ -3,6 +3,7 @@ import speedSolver
 import Options
 import MainScreen
 import TitleScreen
+import Race
 
 def main():
     spyral.director.push(TitleScreen.Title())
