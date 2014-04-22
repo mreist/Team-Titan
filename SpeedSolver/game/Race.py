21bf076ab40d476972c78698d2a688f11fcf838a
Jason <jrub@udel.edu>
2014-04-21 20:21:18 -0400

    Answer input stuff
    Prints correct and incorrect and generates questions, but new question
    won’t show up just yet.

--- SpeedSolver/game/Race.py
+++ SpeedSolver/game/Race.py
@@ -34,13 +34,9 @@ class RaceScene(spyral.Scene):
             AnswerInput = spyral.widgets.TextInput(100, "Answer")
             
         self.my_form = RegisterForm(self)
-        # self.my_form.add_widget.EnterButton(self)
-#         self.my_form.add_widget.AnswerInput(self)
-
         self.my_form.focus()
-        
         self.my_form.QuitButton.pos = ((WIDTH-100), (HEIGHT-50))
-        self.my_form.EnterButton.pos = ((WIDTH/2 + 70), (HEIGHT/2)+400)
+        self.my_form.EnterButton.pos = ((WIDTH/2 + 150), (HEIGHT/2)+400)
         self.my_form.AnswerInput.pos = ((WIDTH/2 + 30), (HEIGHT/2)+400)
         
         spyral.event.register("form.RegisterForm.QuitButton.clicked", self.goToMenu)
@@ -50,18 +46,22 @@ class RaceScene(spyral.Scene):
        
 
     def checkAnswer(self):
-        if self.my_form.AnswerInput.value == currentQuestion.answer:
+        if int(self.my_form.AnswerInput.value) == self.currentQuestion.answer:
             print "CORRECT"
         else:
             print "INCORRECT"
-        self.newQuestion
+            
+        print ("previous answer: " + str(self.currentQuestion.answer))
+        del self.currentQuestion
+        self.currentQuestion = Questions.Question(self, 'addition', 1)
+        print ("new answer: " + str(self.currentQuestion.answer))
         self.level += 1
         if self.level > 10:
             self.goToMenu
             #eventually: go to score screen
             
-    def newQuestion(self):
-        currentQuestion = Questions.Question(self, 'addition', 1)
+#     def newQuestion(self):
+#         p
             
     def goToMenu(self):
         spyral.director.pop
