2ce378aa05507e2e08dff62bb4829fc6207ecedd
acbart <acbart@vt.edu>
2014-05-07 22:19:42 -0400

    Fixed sugargame2 instead of sugargame

--- SpeedSolver/libraries/__init__.py
+++ SpeedSolver/libraries/__init__.py
@@ -3,6 +3,6 @@ import os
 
 def setup_path():
     sys.path.insert(0, os.path.abspath('libraries/'))
-    modules = ['requests', 'envoy', 'sugargame', 'spyral']
+    modules = ['requests', 'envoy', 'sugargame2', 'spyral']
     for module in modules:
         sys.path.insert(0, os.path.abspath('libraries/%s/' % module))
