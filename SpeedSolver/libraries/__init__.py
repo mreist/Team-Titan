b4b009ad39d894dd25f6a2903d0481935c3baaac
acbart <acbart@vt.edu>
2014-05-07 21:09:00 -0400

    Fixed sugargame mis-import

--- SpeedSolver/libraries/__init__.py
+++ SpeedSolver/libraries/__init__.py
@@ -3,6 +3,6 @@ import os
 
 def setup_path():
     sys.path.insert(0, os.path.abspath('libraries/'))
-    modules = ['requests', 'envoy', 'sugargame2', 'spyral']
+    modules = ['requests', 'envoy', 'sugargame', 'spyral']
     for module in modules:
         sys.path.insert(0, os.path.abspath('libraries/%s/' % module))
