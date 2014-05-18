3b41dfc8284c6d7cdc78210212cd6f33a6c87ab5
avellen <avellen@udel.edu>
2014-05-18 11:53:33 -0400

    Adding Comments to Player
    Decribing each class within Player

--- SpeedSolver/game/Player.py
+++ SpeedSolver/game/Player.py
@@ -3,13 +3,20 @@ import Model
 from Model import resources
 from spyral import Animation, easing
 
+#The Players time
 currentTime = 0
 bestTime = 100000
+# Number of tokens a player has. 
+#Tokens allow a player to buy items in the Garage such as new wheels, decays and vehicles.
 tokens = 10
+
 firstPlay = True
+#Top ten players in the leaderboard
 top10 = [['JFR', 1], ['MTR', 30], ['JTW', 60], ['NNA', 75], ['FAP', 1000], ['',1000000], ['',1000000], ['',1000000], ['',1000000], ['',1000000]]
 WithWheels = True
 
+
+#Player Vehicle class that has a Vtype and compares it to the Model.Vtype in order to display a particular vehicle
 class PlayerVehicle(spyral.Sprite):
     def __init__(self, Scene):
         spyral.Sprite.__init__(self, Scene)
@@ -17,7 +24,7 @@ class PlayerVehicle(spyral.Sprite):
         self.Vtype = Model.Vtype
         self.image  = Model.resources[self.Vtype]
         
-
+#Player Decal class that has a Decal and compares it to the Model.Decal in order to display a particular Decal
 class PlayerDecal(spyral.Sprite):
     def __init__(self, Scene):
         spyral.Sprite.__init__(self, Scene)
@@ -25,8 +32,10 @@ class PlayerDecal(spyral.Sprite):
         self.Decal = Model.Decal
         self.image = Model.resources[self.Decal]
 
-if (WithWheels == True):        
-    class PlayerLWheels(spyral.Sprite):
+if (WithWheels == True):
+    
+    #Player LWheel class that has a LWheel and compares it to the Model.LWheel.
+    #In order to display different Lwheel images
         def __init__(self, Scene):
             spyral.Sprite.__init__(self, Scene)
             try:
@@ -36,7 +45,10 @@ if (WithWheels == True):
                 self.image  = Model.resources[self.LWtype]
             except (KeyError, AttributeError):
                 print "Nothing1"
-            
+                
+    #Player RWheel class that has a RWheel and compares it to the Model.RWheel.
+    #In order to display different Rwheel images  
+    
     class PlayerRWheels(spyral.Sprite):
         def __init__(self, Scene):
             spyral.Sprite.__init__(self, Scene)
